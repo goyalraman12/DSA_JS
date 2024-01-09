9baae6891d81f5ed2de1cea75301b681b2b51016
goyalraman12 <55880680+goyalraman12@users.noreply.github.com>
2024-01-09 20:11:43 +0530

    chore: program to print prime factors of a number (#1)
    Co-authored-by: Raman Goyal <ramangoyal@MacBook-Air.local>

---
+++ Mathematics/primeFactors.js
@@ -0,0 +1,43 @@
+function checkPrimeFactors_NAIVE(n) {
+  while (n % 2 === 0) {
+    console.log(2);
+    n = n / 2;
+  }
+
+  // n must be odd at this point. So we can skip
+  // one element (Note i = i + 2)
+  for (let i = 3; i <= Math.sqrt(n); i = i + 2) {
+    // While i divides n, print i and divide n
+    while (n % i === 0) {
+      console.log(i);
+      n = n / i;
+    }
+  }
+
+  // This condition is to handle the case when n
+  // is a prime number greater than 2
+  if (n > 2) console.log(n);
+}
+
+function printPrimeFactors_EFFICIENT(n) {
+  if (n <= 1) return;
+  while (n % 2 === 0) {
+    console.log(2);
+    n = n / 2;
+  }
+  while (n % 3 === 0) {
+    console.log(3);
+    n = n / 3;
+  }
+  for (let i = 5; i * i <= n; i = i + 6) {
+    while (n % i === 0) {
+      console.log(i);
+      n = n / i;
+    }
+    while (n % (i + 2) === 0) {
+      console.log(i + 2);
+      n = n / (i + 2);
+    }
+  }
+  if (n > 3) console.log(n);
+}
